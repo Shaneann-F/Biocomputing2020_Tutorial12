8d8737df8f4c49eb611defa00746a2aa62633ecf
Shaneann Fross <sfross@nd.edu>
2020-11-05 21:38:33 -0500

    Finished finally

--- Exercise_10-Fross.R
+++ Exercise_10-Fross.R
@@ -27,4 +27,26 @@ ggplot(data = Grades,
   stat_smooth(method="lm") +
   theme_classic() 
 
+#Number 2
+##Load the data.txt
+data = read.table("data.txt", header=TRUE, sep=",", stringsAsFactors=FALSE)
+
+##Write a script that generates two figures that summarizes the data
+##Show a barplot of the four populations
+ggplot(data, aes(x = region, y = observations)) +
+  stat_summary(fun = mean, geom = "bar") +
+  xlab("Region") +
+  ylab("Observations") +
+  theme_Lect24()
+
+##Show a scatter plot of all the observations
+ggplot(data, aes(x=region, y=observations)) +
+  geom_jitter() +
+  xlab("Region") +
+  ylab("Observations") +
+  theme_classic()
   
+
+##Do the bar and scatter plots tell different stories? Why?
+#The bar and scatterplots do tell different stories. The bar plot shows the means for each of the regions which all hover around the same number.
+#The scatterplot shows instead where each data point is. While east and west show a pretty spread distriution north is concentrated and south has two discreete populations. The scatter plot offers greater insight into the data.
